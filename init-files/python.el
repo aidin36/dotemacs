34738d64e14dbe4bd98f940d2f128a1f0d03c8f4
Aidin Gharibnavaz <aidin@aidinhut.com>
2018-06-01 10:08:38 +1200

    Moving package requirement inside "my-python" for faster startup.

--- init-files/python.el
+++ init-files/python.el
@@ -1,13 +1,13 @@
-(use-package elpy
-  :ensure t
-)
-
 ;;;
 ;;; Call this to swtich on all Python configs.
 ;;;
 (defun my-python ()
   "Turn on all Python customizations"
   (interactive)
+
+  (use-package elpy
+    :ensure t
+  )
 
   (elpy-enable)
 )
