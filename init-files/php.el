bdd06455a3744e01d05b8bb0b20cd35105ac7df8
Aidin Gharibnavaz <aidin@aidinhut.com>
2018-07-03 14:49:28 +1200

    Adds docker, php, and some funcs.

---
+++ init-files/php.el
@@ -0,0 +1,13 @@
+(use-package php-mode
+  :ensure t
+)
+
+;; Using tab instead of spaces.
+(add-hook 'php-mode-hook 'my-php-mode-hook)
+(defun my-php-mode-hook ()
+  (setq indent-tabs-mode t)
+  (let ((my-tab-width 4))
+    (setq tab-width my-tab-width)
+    (setq c-basic-indent my-tab-width)
+    (set (make-local-variable 'tab-stop-list)
+         (number-sequence my-tab-width 200 my-tab-width))))
--- init.el
+++ init.el
@@ -53,7 +53,7 @@
     ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(package-selected-packages
    (quote
-    (py-autopep8 jedi go-eldoc elpy exec-path-from-shell solarized-theme multiple-cursors xclip flycheck go-mode magit highlight-parentheses popup-imenu helm ensime use-package))))
+    (php-mode dockerfile-mode py-autopep8 jedi go-eldoc elpy exec-path-from-shell solarized-theme multiple-cursors xclip flycheck go-mode magit highlight-parentheses popup-imenu helm ensime use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -181,8 +181,11 @@
 ;;;
 ;;; Other config files.
 ;;;
+(load "~/.emacs.d/init-files/funcs.el")
 (load "~/.emacs.d/init-files/scala.el")
 (load "~/.emacs.d/init-files/git.el")
 (load "~/.emacs.d/init-files/go.el")
 (load "~/.emacs.d/init-files/yaml.el")
 (load "~/.emacs.d/init-files/python.el")
+(load "~/.emacs.d/init-files/docker.el")
+(load "~/.emacs.d/init-files/php.el")
