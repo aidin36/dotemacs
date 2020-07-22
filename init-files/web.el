19eca5b892736f8eb844d641ce10f35398e0027a
Aidin Gharibnavaz <aidin@movio.co>
2020-07-22 17:20:30 +1200

    Remove flow-mode
    It needs 'flow' binary to be installed globally, but that causes
    our 'make flow' to be failed.

--- init-files/web.el
+++ init-files/web.el
@@ -5,17 +5,6 @@
   "Turn on all Python customizations"
   (interactive)
 
-  (use-package flow-minor-mode
-    :ensure t)
-  (use-package flycheck-flow
-    :ensure t)
-
-  ;; Adding flow to the flycheck
-  (with-eval-after-load 'flycheck
-    (flycheck-add-mode 'javascript-flow 'flow-minor-mode)
-    (flycheck-add-mode 'javascript-eslint 'flow-minor-mode)
-    (flycheck-add-next-checker 'javascript-flow 'javascript-eslint))
-
   ;; Enabling fly-check which uses ESLint
   ;; ESLint needs to be installed globally.
   (global-flycheck-mode)
